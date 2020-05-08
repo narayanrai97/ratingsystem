d3827959eea0527b814675a71adb1ca4a20e1884
Narayan Rai <narayanrai97@gmail.com>
2020-05-08 14:42:26 -0400

    Add item model validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
     belongs_to :user
     has_many   :user_ratings
+    validates  :title, :description, :genre, :direction, presence: true
 
     has_attached_file :image, styles: { medium: "400x600#" }
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
